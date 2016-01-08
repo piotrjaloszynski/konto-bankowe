ae1f86d1508859855b3d705dd655cf8721992313
Piotr Jaloszynski <jaloszynskip@yahoo.com>
2016-01-08 13:52:37 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Gosia on 2016-01-08.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Czy dzialasz");
+    }
+}
---
+++ src/bank/Bank.java
@@ -0,0 +1,8 @@
+package bank;
+
+/**
+ * Created by Gosia on 2016-01-08.
+ */
+public class Bank {
+
+}
